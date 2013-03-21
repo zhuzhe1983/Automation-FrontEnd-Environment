47816bf3c4500eb42a689b3624f90fa553731b85
chaoren1641 <chaoren1641@gmail.com>
2013-03-21 19:59:10 +0800

    添加所有文件

---
+++ Gruntfile.js
@@ -0,0 +1,187 @@
+'use strict';
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+var mountFolder = function(connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+
+module.exports = function(grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        watch: {
+            compass: {
+                files: ['app/styles/{,*/}*.{scss,sass}'],
+                tasks: ['compass']
+            },
+            jade: {
+                files: ['app/{,*/}*.jade','app/{,*/}*.html',],
+                tasks: ['jade:debug']
+            },
+            livereload: {
+                files: [
+                    '{.tmp,app}/styles/{,*/}*.css',
+                    '{.tmp,app}/scripts/{,*/}*.js',
+                    '{.tmp,app}/images/{,*/}*.{png,jpg,jpeg,webp}'],
+                tasks: ['livereload']
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                hostname: '0.0.0.0'
+            },
+            livereload: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                        lrSnippet,
+                        mountFolder(connect, '.tmp'),
+                        mountFolder(connect, 'app')];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                        mountFolder(connect, '.tmp'),
+                        mountFolder(connect, 'test')];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                        mountFolder(connect, 'dist')];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        clean: {
+            debug: '.tmp',
+            dist: ['.tmp', 'dist/*'],
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        compass: {
+            debug: {
+               options:{
+                    sassDir: 'app/styles',
+                    cssDir: '.tmp/styles',
+                }
+            },
+            dist: {
+                options:{
+                    sassDir: 'app/styles',
+                    cssDir: 'dist/styles',
+                    environment: 'production'
+                }
+            }
+        },
+        jade: {
+            debug: {
+              options: {
+                data: {
+                  debug: true
+                },
+                pretty:true
+              },
+              files: [{
+                expand: true,     
+                cwd: 'app/',      
+                src: ['**/*.jade'], 
+                dest: '.tmp/',   
+                ext: '.html'
+              }]
+            },
+            dist: {
+              options: {
+                data: {
+                  debug: false
+                }
+              },
+              files: [{
+                expand: true,     
+                cwd: 'app/',      
+                src: ['**/*.jade'], 
+                dest: '.dist/',   
+                ext: '.html'
+              }]
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/scripts/main.js': [
+                        'app/scripts/{,*/}*.js'],
+                }
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/images',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: 'dist/images'
+                }]
+            }
+        },
+        copy: {
+            debug: {
+                files: [
+                  {expand: true, cwd: 'app/images/', src: ['**'], dest: '.tmp/images'}, // makes all src relative to cwd
+                  {expand: true, cwd: 'app/scripts/', src: ['**'], dest: '.tmp/scripts'} // flattens
+                ]
+            },
+            dist: {
+                files: [
+                  {expand: true, cwd: 'app/images/', src: ['**'], dest: 'dist/images'}, // makes all src relative to cwd
+                  {expand: true, cwd: 'app/scripts/', src: ['**'], dest: 'dist/scripts'} // flattens
+                ]
+            }
+        }
+    });
+
+    grunt.renameTask('regarde', 'watch');
+
+    grunt.registerTask('server', function(target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:dist',
+            'compass:debug',
+            'jade:debug',
+            'livereload-start',
+            'connect:livereload',
+            'copy:debug',
+            'open',
+            'watch']);
+    });
+
+
+    grunt.registerTask('build', [
+        'clean:debug',
+        'clean:dist',
+        'compass:dist',
+        'jade:dist',
+        'imagemin',
+        'uglify',
+        'copy:dist',]);
+
+    grunt.registerTask('default', [
+        'server']);
+};
---
+++ README.md
@@ -0,0 +1,44 @@
+
+## 项目介绍
+这是一个自动化的前端环境，
+采用[Grunt](http://gruntjs.com) + [sass](http://sass-lang.com) + [compass](http://compass-style.org/) + [jade](http://jade-lang.com/) 搭建。此环境适合多个前端开发人员协作开发，快速编写HTML与CSS。
+
+## 安装
+因为sass和compass依赖ruby环境，所以你首先需要[安装ruby](http://www.ruby-lang.org/en/downloads/)，安装好以后执行
+
+```
+gem install compass
+```
+然后从这里[下载NodeJS的安装包](http://nodejs.org/),安装NodeJS.
+
+最后安装Grunt-cli
+
+```
+npm install -g grunt-cli
+```
+
+这时候你的环境就安装好了。
+
+下载所有源代码，然后在当前目录下使用NodeJS命令
+
+```
+npm install
+```
+
+## 运行
+
+安装好以后执行
+
+```
+grunt
+```
+浏览器将自动打开预览地址。
+
+## 备注：
+[sass用法指南](http://www.ruanyifeng.com/blog/2012/06/sass.html)
+
+[Compass用法指南](http://www.ruanyifeng.com/blog/2012/11/compass.html)
+
+[Jade模板引擎入门教程](http://www.csser.com/board/4f3f516e38a5ebc978000508)
+
+这三个东东不用担心，很快就能学会。学好之后可以让你事半功倍哦！！
