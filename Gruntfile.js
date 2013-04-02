5205e21dade1b35702fd63e4c9f2e1da82978c02
chaoren1641 <chaoren1641@gmail.com>
2013-04-02 14:00:19 +0800

    gruntfile代码错误更正

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,8 +16,12 @@ module.exports = function(grunt) {
                 tasks: ['compass']
             },
             jade: {
-                files: ['app/{,*/}*.jade','app/{,*/}*.html',],
+                files: ['app/{,*/}*.jade', 'app/{,*/}*.html'],
                 tasks: ['jade:debug']
+            },
+            copy: {
+                files: ['app/scripts/{,*/}*.js', 'app/images/{,*/}*.{png,jpg,jpeg,webp}'],
+                tasks: ['copy:debug']
             },
             livereload: {
                 files: [
@@ -42,12 +46,12 @@ module.exports = function(grunt) {
                     }
                 }
             },
-            test: {
+            debug: {
                 options: {
                     middleware: function(connect) {
                         return [
                         mountFolder(connect, '.tmp'),
-                        mountFolder(connect, 'test')];
+                        mountFolder(connect, 'app')];
                     }
                 }
             },
@@ -72,52 +76,57 @@ module.exports = function(grunt) {
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
-            }
+            },
+            all: [
+                'app/scripts/{,*/}*.js']
         },
         compass: {
             debug: {
-               options:{
+                options: {
                     sassDir: 'app/styles',
                     cssDir: '.tmp/styles',
+                    imagesDir: 'app/images'
                 }
             },
             dist: {
-                options:{
+                options: {
                     sassDir: 'app/styles',
                     cssDir: 'dist/styles',
+                    imagesDir: 'app/images',
                     environment: 'production'
                 }
             }
         },
         jade: {
             debug: {
-              options: {
-                data: {
-                  debug: true
+                options: {
+                    data: {
+                        debug: true
+                    },
+                    pretty: true
                 },
-                pretty:true
-              },
-              files: [{
-                expand: true,     
-                cwd: 'app/',      
-                src: ['**/*.jade'], 
-                dest: '.tmp/',   
-                ext: '.html'
-              }]
+                files: [{
+                    expand: true,
+                    cwd: 'app/',
+                    src: ['**/*.jade'],
+                    dest: '.tmp/',
+                    ext: '.html'
+                }]
             },
             dist: {
-              options: {
-                data: {
-                  debug: false
-                }
-              },
-              files: [{
-                expand: true,     
-                cwd: 'app/',      
-                src: ['**/*.jade'], 
-                dest: '.dist/',   
-                ext: '.html'
-              }]
+                options: {
+                    data: {
+                        debug: true
+                    },
+                    pretty: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/',
+                    src: ['**/*.jade'],
+                    dest: 'dist/',
+                    ext: '.html'
+                }]
             }
         },
         uglify: {
@@ -140,15 +149,33 @@ module.exports = function(grunt) {
         },
         copy: {
             debug: {
-                files: [
-                  {expand: true, cwd: 'app/images/', src: ['**'], dest: '.tmp/images'}, // makes all src relative to cwd
-                  {expand: true, cwd: 'app/scripts/', src: ['**'], dest: '.tmp/scripts'} // flattens
+                files: [{
+                    expand: true,
+                    cwd: 'app/images/',
+                    src: ['**'],
+                    dest: '.tmp/images'
+                }, // makes all src relative to cwd
+                {
+                    expand: true,
+                    cwd: 'app/scripts/',
+                    src: ['**'],
+                    dest: '.tmp/scripts'
+                } // flattens
                 ]
             },
             dist: {
-                files: [
-                  {expand: true, cwd: 'app/images/', src: ['**'], dest: 'dist/images'}, // makes all src relative to cwd
-                  {expand: true, cwd: 'app/scripts/', src: ['**'], dest: 'dist/scripts'} // flattens
+                files: [{
+                    expand: true,
+                    cwd: 'app/images/',
+                    src: ['**'],
+                    dest: 'dist/images'
+                }, // makes all src relative to cwd
+                {
+                    expand: true,
+                    cwd: 'app/scripts/',
+                    src: ['**'],
+                    dest: 'dist/scripts'
+                } // flattens
                 ]
             }
         }
@@ -169,6 +196,7 @@ module.exports = function(grunt) {
             'connect:livereload',
             'copy:debug',
             'open',
+            'jshint',
             'watch']);
     });
 
@@ -180,7 +208,7 @@ module.exports = function(grunt) {
         'jade:dist',
         'imagemin',
         'uglify',
-        'copy:dist',]);
+        'copy:dist']);
 
     grunt.registerTask('default', [
         'server']);
